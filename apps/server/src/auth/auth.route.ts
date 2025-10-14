b10d2a373f2844c78897de9038a40caedbbb1aaa
Jovan <jovandjukic2@gmail.com>
2025-10-14 18:28:06 +0200

    feat(auth): secure logout route with JWT authentication middleware
    Added passport JWT strategy to protect the `/logout` endpoint.

--- apps/server/src/auth/auth.route.ts
+++ apps/server/src/auth/auth.route.ts
@@ -1,8 +1,13 @@
 import express from "express";
 import { authController } from "./index.js";
+import passport from "./jwt/jwt.strategy.ts";
 
 export const authRouter = express.Router();
 
 authRouter.post("/register", authController.register.bind(authController));
 authRouter.post("/login", authController.login.bind(authController));
-authRouter.delete("/logout", authController.delete.bind(authController));
+authRouter.delete(
+  "/logout",
+  passport.authenticate("jwt", { session: false }),
+  authController.delete.bind(authController)
+);
