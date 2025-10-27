6d1ef7bbaf4ea4c34ebee949686d78c35a7d2841
Jovan <jovandjukic2@gmail.com>
2025-10-27 18:44:02 +0100

    chore(seeds): generate randomized users with faker for test data
    - Added @faker-js/faker to devDependencies.
    - Replaced static user seeds with dynamic faker-based generation.
    - Added createRandomUser() helper with secure password hashing.
    - Inserts 10 randomized users for improved test coverage.

--- apps/server/seeds/users.ts
+++ apps/server/seeds/users.ts
@@ -1,22 +1,36 @@
+import { faker } from "@faker-js/faker";
 import type { Knex } from "knex";
 import { hashing } from "../lib/index.ts";
 
+async function createRandomUser(): Promise<{
+	email: string;
+	first_name: string;
+	last_name: string;
+	password: string;
+}> {
+	const sex = faker.person.sexType();
+	const firstName = faker.person.firstName(sex);
+	const lastName = faker.person.lastName(sex);
+	const email = faker.internet.email({ firstName, lastName });
+	const password = faker.internet.password({ memorable: true });
+
+	return {
+		email,
+		first_name: firstName,
+		last_name: lastName,
+		password: await hashing.hash(password),
+	};
+}
+
 export async function seed(knex: Knex): Promise<void> {
-	await knex("users").insert([
-		{
-			email: "test1@mail.com",
-			password: await hashing.hash("test1"),
-			role_id: 2,
-		},
-		{
-			email: "test2@mail.com",
-			password: await hashing.hash("test2"),
-			role_id: 2,
-		},
-		{
-			email: "test3@mail.com",
-			password: await hashing.hash("test3"),
-			role_id: 2,
-		},
-	]);
+	const users = await Promise.all(
+		Array.from({ length: 10 }, async () => {
+			return {
+				...(await createRandomUser()),
+				role_id: 2,
+			};
+		}),
+	);
+
+	await knex("users").insert(users);
 }
